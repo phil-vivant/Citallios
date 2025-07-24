6e5f4c168fd71728cf5b621d48dddefe3f4adbec
phil-vivant <philippe.vivant@gmail.com>
2025-07-24 17:52:20 +0200

    modif generale

--- Citallios.py
+++ Citallios.py
@@ -3,12 +3,13 @@
 # Third party imports
 
 # Local applications imports
-from moteur import calcul_section
+from moteur import def_sections, print_hypotheses, verif_elu, verif_els, verif_feu
 
 
 
 materiaux = {
-    'fck': 25, 
+    'fck': 25,
+    'class_acier': "B",
     'fyk': 500,
     'Ef': 170_000,
 }
@@ -17,9 +18,16 @@ geom = {
     'h_dalle': 0.25,
     'b_dalle': 2.60,
     'As': 1.13e-4,
-    'er_s': 0.04,
+    'dprim_s': 0.04,
     'ns': 4,
-    'Af': 0.96e-4,
+}
+
+renforts = {
+    'Asr': 1.13e-4,
+    'dprim_sr': 0.025,
+    'nsr': 2,
+    'Af': 0.906e-4,
+    'dprim_f': 0,
     'nf': 3,
 }
 
@@ -27,6 +35,14 @@ efforts = {
     'm_els_1': 30,
     'm_els_2': 30,
     'm_elu': 80,
+    'm_feu': 60,
 }
 
-calcul_section(materiaux=materiaux, geometrie=geom, efforts=efforts)
+print_hypotheses(
+    materiaux=materiaux, geometrie=geom, renforts=renforts, efforts=efforts,
+)
+verif_els(materiaux=materiaux, geometrie=geom, renforts=renforts, efforts=efforts)
+verif_elu(materiaux=materiaux, geometrie=geom, renforts=renforts, efforts=efforts)
+verif_feu(materiaux=materiaux, geometrie=geom, renforts=renforts, efforts=efforts)
+# section_1.plot_interaction_diagram_v2(finess=1)
+# section_2.plot_interaction_diagram_v2(finess=1)
--- moteur.py
+++ moteur.py
@@ -21,31 +21,63 @@ start_time = time.time()
 
 
 
-def calcul_section(materiaux: dict[str: float], geometrie: dict[str: float], efforts: dict[str: float]):
-
-    # Unpack materials
+def unpack_materiaux(materiaux: dict[str: float]):
     fck = materiaux["fck"]
+    class_acier = materiaux["class_acier"]
     fyk = materiaux["fyk"]
     Ef = materiaux["Ef"]
+    return fck, class_acier, fyk, Ef
 
-    # Unpack geometry
+
+def unpack_geometry(geometrie: dict[str: float]):
     h_dalle = geometrie["h_dalle"]
     b_dalle = geometrie["b_dalle"]
     As = geometrie["As"]
-    er_s = geometrie["er_s"]
+    dprim_s = geometrie["dprim_s"]
     ns = geometrie["ns"]
-    Af = geometrie["Af"]
-    nf = geometrie["nf"]
+    return h_dalle, b_dalle, As, dprim_s, ns
 
-    # Unpack forces
+def unpack_renforts(renforts: dict[str: float]):
+    Asr = renforts["Asr"]
+    dprim_sr = renforts["dprim_sr"]
+    nsr = renforts["nsr"]
+    Af = renforts["Af"]
+    dprim_f = renforts["dprim_f"]
+    nf = renforts["nf"]
+    return Asr, dprim_sr, nsr, Af, dprim_f, nf
+
+def unpack_forces(efforts: dict[str: float]):
     m_els_1 = efforts["m_els_1"]
     m_els_2 = efforts["m_els_2"]
     m_elu = efforts["m_elu"]
+    m_feu = efforts["m_feu"]
+    return m_els_1, m_els_2, m_elu, m_feu
+
+
+def def_sections(
+        materiaux: dict[str: float],
+        geometrie: dict[str: float],
+        renforts: dict[str: float],
+        comb: str='elu',
+):
+    # Unpack Data
+    fck, class_acier, fyk, Ef = unpack_materiaux(materiaux)
+    h_dalle, b_dalle, As, dprim_s, ns = unpack_geometry(geometrie=geometrie)
+    Asr, dprim_sr, nsr, Af, dprim_f, nf = unpack_renforts(renforts=renforts)
 
     # Création des matériaux
-    concrete = EC2Concrete(fck=fck, diagram_type="uls_parabola", gamma_c=1.5)
-    acier = SteelRebar("B", yield_strength_fyk=fyk, gamma_s=1.15)
-    renf_carbone = FibreReinforcedPolymer(modulus_elasticity_ef=Ef)
+    if comb.lower() == 'elu':
+        concrete = EC2Concrete(fck=fck, diagram_type="uls_parabola", gamma_c=1.5)
+        acier = SteelRebar(ductility_class=class_acier, yield_strength_fyk=fyk, gamma_s=1.15)
+        renf_carbone = FibreReinforcedPolymer(modulus_elasticity_ef=Ef)
+    if comb.lower() == 'els':
+        concrete = EC2Concrete(fck=fck, diagram_type="sls_cracked")
+        acier = SteelRebar(ductility_class=class_acier, yield_strength_fyk=fyk, diagram_tpe="sls")
+        renf_carbone = FibreReinforcedPolymer(modulus_elasticity_ef=Ef)
+    if comb.lower() == 'feu':
+        concrete = EC2Concrete(fck=fck, diagram_type="uls_parabola", gamma_c=1)
+        acier = SteelRebar(ductility_class=class_acier, yield_strength_fyk=fyk, gamma_s=1)
+        renf_carbone = FibreReinforcedPolymer(modulus_elasticity_ef=1e-9)
 
     # Géométrie Dalle
     pt_00 = Point(-b_dalle / 2, 0.000)
@@ -57,7 +89,7 @@ def calcul_section(materiaux: dict[str: float], geometrie: dict[str: float], eff
     dalle_reg = Region(0, [dalle_poly], concrete)
 
     # Ferraillage de la dalle
-    ys = er_s
+    ys = dprim_s
     HA_pts = []
     es = b_dalle / ns
     xs = -es * (ns - 1) / 2
@@ -75,50 +107,95 @@ def calcul_section(materiaux: dict[str: float], geometrie: dict[str: float], eff
         fibre_size_z=0.005,
     )
 
-    FRP_pts = []
-    ef = b_dalle / nf
-    xf = -ef * (nf - 1) / 2
-    for i in range(nf):
-        FRP_pts.append(Point(xf, 0))
-        xf += ef
+    # Acier de renforcement
+    rebars=[rebar_inf]
+    if Asr * nsr > 0:
+        Asr_pts = []
+        esr = b_dalle / nsr
+        xsr = -esr * (nsr - 1) / 2
+        for i in range(nsr):
+            Asr_pts.append(Point(xsr, 0))
+            xsr += esr
+        rebar_renf = Rebars(0, Asr, acier, Asr_pts, 31)
+        rebars.append(rebar_renf)
 
-    FRP_inf = FRPStrips(0, Af, renf_carbone, FRP_pts, 1)
+    # FRP de renforcement
+    if Af * nf > 0:
+        FRP_pts = []
+        ef = b_dalle / nf
+        xf = -ef * (nf - 1) / 2
+        for i in range(nf):
+            FRP_pts.append(Point(xf, 0))
+            xf += ef
+        FRP_inf = FRPStrips(0, Af, renf_carbone, FRP_pts, 1)
+    else:
+        dummy_frp = FibreReinforcedPolymer(1e-9, 10000, 10000)
+        FRP_inf = FRPStrips(0, 0, dummy_frp, [Point(0, 0)], 1)
 
     dalle_renf = ConcreteSection(
         regions=[dalle_reg],
-        rebars=[rebar_inf],
+        rebars=rebars,
         frp_strips=[FRP_inf],
         fibre_size_y=0.01,
         fibre_size_z=0.005,
     )
 
+    return dalle, dalle_renf
+
+
+def print_hypotheses(
+        materiaux: dict[str: float],
+        geometrie: dict[str: float],
+        renforts: dict[str: float],
+        efforts: dict[str: float],
+):
+    # Unpack Data
+    fck, class_acier, fyk, Ef = unpack_materiaux(materiaux)
+    h_dalle, b_dalle, As, dprim_s, ns = unpack_geometry(geometrie=geometrie)
+    Asr, dprim_sr, nsr, Af, dprim_f, nf = unpack_renforts(renforts=renforts)
+    m_els_1, m_els_2, m_elu, m_feu = unpack_forces(efforts=efforts)
+
+    # Présentation Hypothèses
     data = "Rappel des hypothèses :"
     data += f"\n\tSection : \t{round(b_dalle, 2)} m x {round(h_dalle, 2)} m ht"
-    data += f"\n\tBéton :\t\tfck = {concrete.fck} MPa"
-    data += f"\n\tAcier HA :\tfyk = {acier.yield_strength_fyk} MPa"
+    data += f"\n\tBéton :\t\tfck = {fck} MPa"
+    data += f"\n\tAcier HA :\tfyk = {fyk} MPa"
     data += f"\n\tBarres HA : \t{ns} x As = {round(As * 1e4, 2)} cm²"
+    data += f"\n\tRenforts HA : \t{nsr} x Asr = {round(Asr * 1e4, 2)} cm²"
     data += f"\n\tRenforts FRP : \t{nf} x Af = {round(Af * 1e4, 2)} cm²"
     data += f"\n\nRappel des sollicitations :"
     data += f"\n\tM_ELS1 = {round(m_els_1, 2)} kN.m"
     data += f"\n\tM_ELS2 = {round(m_els_2, 2)} kN.m"
     data += f"\n\tM_ELU = {round(m_elu, 2)} kN.m"
+    data += f"\n\tM_feu = {round(m_feu, 2)} kN.m"
     print(data)
 
+    return None
+
+
+def verif_elu(
+        materiaux: dict[str: float],
+        geometrie: dict[str: float],
+        renforts: dict[str: float],
+        efforts: dict[str: float],
+):
+    section_1, section_2 = def_sections(materiaux, geometrie, renforts, comb='elu')
     ned = 0
+    m_els_1, m_els_2, m_elu, m_feu = unpack_forces(efforts=efforts)
     result_elu = "Vérification ELU :"
     result_elu += f"\n\tMoment sollicitant : \tM_ed = {m_elu: .1f} kN.m"
-    result_elu += f"\n\tAvant renforcement : \tM_rd1 = {dalle.Mrd_max(ned): .1f} kN.m"
-    result_elu += f"\n\tAprès renforcement : \tM_rd2 = {dalle_renf.Mrd_max(ned): .1f} kN.m"
+    result_elu += f"\n\tAvant renforcement : \tM_rd1 = {section_1.Mrd_max(ned): .1f} kN.m"
+    result_elu += f"\n\tAprès renforcement : \tM_rd2 = {section_2.Mrd_max(ned): .1f} kN.m"
     print(result_elu)
 
-    pod_elu = dalle_renf.from_forces_to_curvature(0, m_elu, 0)
-    frp_state_elu = dalle_renf.frp_internal_state(pod_elu)
+    pod_elu = section_2.from_forces_to_curvature(0, m_elu, 0)
+    frp_state_elu = section_2.frp_internal_state(pod_elu)
     sigma_frp_elu = next(iter(frp_state_elu.values()))['stress']
-    concrete_state_elu = dalle_renf.concrete_internal_state(pod_elu)
+    concrete_state_elu = section_2.concrete_internal_state(pod_elu)
     sigma_conc_elu = 0
     for fibre in concrete_state_elu.values():
         sigma_conc_elu = max(sigma_conc_elu, fibre["stress"])
-    rebars_state_elu = dalle.rebars_internal_state(pod_elu)
+    rebars_state_elu = section_2.rebars_internal_state(pod_elu)
     sigma_rebar_elu = next(iter(rebars_state_elu.values()))['stress']
 
     equilibre_elu = "Equilibre de la section renforcée à l'ELU :"
@@ -127,17 +204,61 @@ def calcul_section(materiaux: dict[str: float], geometrie: dict[str: float], eff
     equilibre_elu += f"\n\tContrainte carbone : \tσ_f = {round(sigma_frp_elu, 1)} MPa"
     print(equilibre_elu)
 
-    pod_1 = dalle.from_forces_to_curvature(0, m_els_1, 0)
-    pod_2 = dalle_renf.from_forces_to_curvature(0, m_els_2, 0)
+    return None
 
-    concrete.set_diagram_type('sls_cracked')
-    acier.set_diagram_type('sls')
 
-    concrete_state_1 = dalle.concrete_internal_state(pod_1)
+def verif_feu(
+        materiaux: dict[str: float],
+        geometrie: dict[str: float],
+        renforts: dict[str: float],
+        efforts: dict[str: float],
+):
+    section_1, section_2 = def_sections(materiaux, geometrie, renforts, comb='feu')
+    ned = 0
+    m_els_1, m_els_2, m_elu, m_feu = unpack_forces(efforts=efforts)
+    result_elu = "Vérification au feu :"
+    result_elu += f"\n\tMoment sollicitant : \tM_ed = {m_feu: .1f} kN.m"
+    result_elu += f"\n\tAvant renforcement : \tM_rd1 = {section_1.Mrd_max(ned): .1f} kN.m"
+    result_elu += f"\n\tAprès renforcement : \tM_rd2 = {section_2.Mrd_max(ned): .1f} kN.m"
+    print(result_elu)
+
+    pod_feu = section_2.from_forces_to_curvature(0, m_feu, 0)
+    frp_state_feu = section_2.frp_internal_state(pod_feu)
+    sigma_frp_feu = next(iter(frp_state_feu.values()))['stress']
+    concrete_state_feu = section_2.concrete_internal_state(pod_feu)
+    sigma_conc_feu = 0
+    for fibre in concrete_state_feu.values():
+        sigma_conc_feu = max(sigma_conc_feu, fibre["stress"])
+    rebars_state_feu = section_2.rebars_internal_state(pod_feu)
+    sigma_rebar_feu = next(iter(rebars_state_feu.values()))['stress']
+
+    equilibre_feu = "Equilibre de la section renforcée au feu :"
+    equilibre_feu += f"\n\tContrainte béton : \tσ_c = {round(sigma_conc_feu, 2)} MPa"
+    equilibre_feu += f"\n\tContrainte acier : \tσ_s = {round(sigma_rebar_feu, 1)} MPa"
+    equilibre_feu += f"\n\tContrainte carbone : \tσ_f = {round(sigma_frp_feu, 1)} MPa"
+    print(equilibre_feu)
+
+    return None
+
+
+def verif_els(
+        materiaux: dict[str: float],
+        geometrie: dict[str: float],
+        renforts: dict[str: float],
+        efforts: dict[str: float],
+):
+    section_1, section_2 = def_sections(materiaux, geometrie, renforts, comb='feu')
+    ned = 0
+    m_els_1, m_els_2, m_elu, m_feu = unpack_forces(efforts=efforts)
+
+    pod_1 = section_1.from_forces_to_curvature(0, m_els_1, 0)
+    pod_2 = section_2.from_forces_to_curvature(0, m_els_2, 0)
+
+    concrete_state_1 = section_1.concrete_internal_state(pod_1)
     sigma_conc_1 = 0
     for fibre in concrete_state_1.values():
         sigma_conc_1 = max(sigma_conc_1, fibre["stress"])
-    rebars_state_1 = dalle.rebars_internal_state(pod_1)
+    rebars_state_1 = section_1.rebars_internal_state(pod_1)
     sigma_rebar_1 = next(iter(rebars_state_1.values()))['stress']
 
     bilan_phase_1 = "Phase 1  - État de contraintes ELS dans la section :"
@@ -146,13 +267,13 @@ def calcul_section(materiaux: dict[str: float], geometrie: dict[str: float], eff
     bilan_phase_1 += f"\n\tContrainte carbone :\t σ_f1 = {round(0.00, 2)} MPa"
     print(bilan_phase_1)
 
-    concrete_state_2 = dalle_renf.concrete_internal_state(pod_2)
+    concrete_state_2 = section_2.concrete_internal_state(pod_2)
     sigma_conc_2 = 0
     for fibre in concrete_state_2.values():
         sigma_conc_2 = max(sigma_conc_2, fibre["stress"])
-    rebars_state_2 = dalle_renf.rebars_internal_state(pod_2)
+    rebars_state_2 = section_2.rebars_internal_state(pod_2)
     sigma_rebar_2 = next(iter(rebars_state_2.values()))['stress']
-    frp_state_2 = dalle_renf.frp_internal_state(pod_2)
+    frp_state_2 = section_2.frp_internal_state(pod_2)
     sigma_frp_2 = next(iter(frp_state_2.values()))['stress']
 
     bilan_phase_2 = "Phase 2  - État de contraintes ELS dans la section :"
@@ -167,9 +288,9 @@ def calcul_section(materiaux: dict[str: float], geometrie: dict[str: float], eff
     bilan_final += f"\n\tContrainte carbone :\t σ_f = {round(sigma_frp_2, 2)} MPa"
     print(bilan_final)
 
-    concrete.set_diagram_type('uls_sargin')
-    acier.set_diagram_type('uls')   
+    return None
 
+def test():
     diag_no_renf = dalle.build_NM_interaction_diagram(0, 1)
     courbe_add = []
     for ele in diag_no_renf:
