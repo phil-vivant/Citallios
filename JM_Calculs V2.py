a4880fc9fd3cb9770b3dc32bdb6ea6722a3001ff
phil-vivant <philippe.vivant@gmail.com>
2025-11-20 18:30:23 +0100

    database JM

--- JM_Calculs V2.py
+++ JM_Calculs V2.py
@@ -677,8 +677,8 @@ if __name__ == "__main__":
     # PATH = r"D:\Python\Citallios\Calculs\PH2\DataBase_PH2_FileB_MidStrip_V5.xlsx"
     #__________________________________________________________________________________________________________________________
     # Chemin PH2 TFC avec mise à jour efforts reels etabs + redistribution ELS
-    # PATH = r"D:\Python\Citallios\Calculs\PH2\DataBase_PH2_FileA_V6.xlsx"
-    PATH = r"D:\Python\Citallios\Calculs\PH2\DataBase_PH2_FileB_V6.xlsx"
+    PATH = r"D:\Python\Citallios\Calculs\PH2\DataBase_PH2_FileA_V6.xlsx"
+    # PATH = r"D:\Python\Citallios\Calculs\PH2\DataBase_PH2_FileB_V6.xlsx"
     # PATH = r"D:\Python\Citallios\Calculs\PH2\DataBase_PH2_FileA_MidStrip_V6.xlsx"
     # PATH = r"D:\Python\Citallios\Calculs\PH2\DataBase_PH2_FileB_MidStrip_V6.xlsx"
 
